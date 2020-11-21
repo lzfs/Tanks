6027b40733f2ebdede102ba37a50e439d05e7c48
Laurenz Fuchs <laurenz.fuchs@gmail.com>
2020-11-21 17:13:22 +0100

    update AStar and COMEnemy behaviour

--- Projekte/Tanks/src/main/java/pp/tanks/model/AStar.java
+++ Projekte/Tanks/src/main/java/pp/tanks/model/AStar.java
@@ -6,213 +6,198 @@ import pp.util.DoubleVec;
 import java.util.*;
 
 public class AStar {
-    public final int DIAGONAL_COST;
-    public final int V_H_COST;
+    public static final int DIAGONAL_COST = 14;
+    public static final int V_H_COST = 10;
 
-    public AStar() {
-        this.DIAGONAL_COST = 1;
-        this.V_H_COST = 1;
-    }
-
-    static class Cell{
+    static class Cell {
         int heuristicCost = 0; //Heuristic cost
         int finalCost = 0; //G+H
         int i, j;
         Cell parent;
 
-        Cell(int i, int j){
+        Cell(int i, int j) {
             this.i = i;
             this.j = j;
         }
 
         @Override
-        public String toString(){
-            return "["+this.i+", "+this.j+"]";
+        public String toString() {
+            return "[" + this.i + ", " + this.j + "]";
         }
     }
 
     //Blocked cells are just null Cell values in grid
-    static Cell [][] grid = new Cell[5][5];
+    static Cell[][] grid = new Cell[5][5];
 
     static PriorityQueue<Cell> open;
 
-    static boolean[][] closed;
+    static boolean closed[][];
     static int startI, startJ;
     static int endI, endJ;
 
-    /**
-     * TODO: add JavaDoc
-     * @param i
-     * @param j
-     */
-    public void setBlocked(int i, int j){
+    public static void setBlocked(int i, int j) {
         grid[i][j] = null;
     }
 
-    /**
-     * TODO: add JavaDoc
-     * @param i
-     * @param j
-     */
-    public void setStartCell(int i, int j){
+    public static void setStartCell(int i, int j) {
         startI = i;
         startJ = j;
     }
 
-    /**
-     * TODO: add JavaDoc
-     * @param i
-     * @param j
-     */
-    public void setEndCell(int i, int j){
+    public static void setEndCell(int i, int j) {
         endI = i;
         endJ = j;
     }
 
-    /**
-     * TODO: add JavaDoc
-     * @param current
-     * @param t
-     * @param cost
-     */
-    static void checkAndUpdateCost(Cell current, Cell t, int cost){
-        if(t == null || closed[t.i][t.j])return;
-        int t_final_cost = t.heuristicCost+cost;
+    static void checkAndUpdateCost(Cell current, Cell t, int cost) {
+        if (t == null || closed[t.i][t.j]) return;
+        int t_final_cost = t.heuristicCost + cost;
 
         boolean inOpen = open.contains(t);
-        if(!inOpen || t_final_cost<t.finalCost){
+        if (!inOpen || t_final_cost < t.finalCost) {
             t.finalCost = t_final_cost;
             t.parent = current;
-            if(!inOpen)open.add(t);
+            if (!inOpen) open.add(t);
         }
     }
 
-    /**
-     * TODO: add JavaDoc
-     */
-    public void aStar(){
+    public static void AStar() {
+
+        //add the start location to open list.
         open.add(grid[startI][startJ]);
 
         Cell current;
 
-        while(true){
+        while (true) {
             current = open.poll();
-            if(current==null)break;
-            closed[current.i][current.j]=true;
+            if (current == null) break;
+            closed[current.i][current.j] = true;
 
-            if(current.equals(grid[endI][endJ])){
+            if (current.equals(grid[endI][endJ])) {
                 return;
             }
 
             Cell t;
-            if(current.i-1>=0){
-                t = grid[current.i-1][current.j];
-                checkAndUpdateCost(current, t, current.finalCost+V_H_COST);
+            if (current.i - 1 >= 0) {
+                t = grid[current.i - 1][current.j];
+                checkAndUpdateCost(current, t, current.finalCost + V_H_COST);
 
-                if(current.j-1>=0){
-                    t = grid[current.i-1][current.j-1];
-                    checkAndUpdateCost(current, t, current.finalCost+DIAGONAL_COST);
+                if (current.j - 1 >= 0) {
+                    t = grid[current.i - 1][current.j - 1];
+                    checkAndUpdateCost(current, t, current.finalCost + DIAGONAL_COST);
                 }
 
-                if(current.j+1<grid[0].length){
-                    t = grid[current.i-1][current.j+1];
-                    checkAndUpdateCost(current, t, current.finalCost+DIAGONAL_COST);
+                if (current.j + 1 < grid[0].length) {
+                    t = grid[current.i - 1][current.j + 1];
+                    checkAndUpdateCost(current, t, current.finalCost + DIAGONAL_COST);
                 }
             }
 
-            if(current.j-1>=0){
-                t = grid[current.i][current.j-1];
-                checkAndUpdateCost(current, t, current.finalCost+V_H_COST);
+            if (current.j - 1 >= 0) {
+                t = grid[current.i][current.j - 1];
+                checkAndUpdateCost(current, t, current.finalCost + V_H_COST);
             }
 
-            if(current.j+1<grid[0].length){
-                t = grid[current.i][current.j+1];
-                checkAndUpdateCost(current, t, current.finalCost+V_H_COST);
+            if (current.j + 1 < grid[0].length) {
+                t = grid[current.i][current.j + 1];
+                checkAndUpdateCost(current, t, current.finalCost + V_H_COST);
             }
 
-            if(current.i+1<grid.length){
-                t = grid[current.i+1][current.j];
-                checkAndUpdateCost(current, t, current.finalCost+V_H_COST);
+            if (current.i + 1 < grid.length) {
+                t = grid[current.i + 1][current.j];
+                checkAndUpdateCost(current, t, current.finalCost + V_H_COST);
 
-                if(current.j-1>=0){
-                    t = grid[current.i+1][current.j-1];
-                    checkAndUpdateCost(current, t, current.finalCost+DIAGONAL_COST);
+                if (current.j - 1 >= 0) {
+                    t = grid[current.i + 1][current.j - 1];
+                    checkAndUpdateCost(current, t, current.finalCost + DIAGONAL_COST);
                 }
 
-                if(current.j+1<grid[0].length){
-                    t = grid[current.i+1][current.j+1];
-                    checkAndUpdateCost(current, t, current.finalCost+DIAGONAL_COST);
+                if (current.j + 1 < grid[0].length) {
+                    t = grid[current.i + 1][current.j + 1];
+                    checkAndUpdateCost(current, t, current.finalCost + DIAGONAL_COST);
                 }
             }
         }
     }
 
-    /**
-     * TODO: add JavaDoc
-     * @param x
-     * @param y
-     * @param si
-     * @param sj
-     * @param ti
-     * @param tj
-     * @param blocked
-     * @return
-     */
-    //si, sj = start pos
-    //ti, tj = end pos
-    public ArrayList<DoubleVec> getPath(int x, int y, int si, int sj, int ti, int tj, int[][] blocked){
+    /*
+    Params :
+    tCase = test case No.
+    x, y = Board's dimensions
+    si, sj = start location's x and y coordinates
+    ei, ej = end location's x and y coordinates
+    int[][] blocked = array containing inaccessible cell coordinates
+    */
+    public static List<DoubleVec> execute(int x, int y, int si, int sj, int ei, int ej, int[][] blocked) {
+        //Reset
         grid = new Cell[x][y];
         closed = new boolean[x][y];
         open = new PriorityQueue<>((Object o1, Object o2) -> {
-            Cell c1 = (Cell)o1;
-            Cell c2 = (Cell)o2;
+            Cell c1 = (Cell) o1;
+            Cell c2 = (Cell) o2;
 
-            return Integer.compare(c1.finalCost, c2.finalCost);
+            return c1.finalCost < c2.finalCost ? -1 :
+                    c1.finalCost > c2.finalCost ? 1 : 0;
         });
+        //Set start position
+        setStartCell(si, sj);  //Setting to 0,0 by default. Will be useful for the UI part
 
-        setStartCell(si, sj);
-        setEndCell(ti, tj);
+        //Set End Location
+        setEndCell(ei, ej);
 
-        for(int i=0;i<x;++i){
-            for(int j=0;j<y;++j){
+        for (int i = 0; i < x; ++i) {
+            for (int j = 0; j < y; ++j) {
                 grid[i][j] = new Cell(i, j);
-                grid[i][j].heuristicCost = Math.abs(i-endI)+Math.abs(j-endJ);
+                grid[i][j].heuristicCost = Math.abs(i - endI) + Math.abs(j - endJ);
+//                  System.out.print(grid[i][j].heuristicCost+" ");
             }
+//              System.out.println();
         }
         grid[si][sj].finalCost = 0;
-
-        for (int[] ints : blocked) {
-            setBlocked(ints[0], ints[1]);
+           
+           /*
+             Set blocked cells. Simply set the cell values to null
+             for blocked cells.
+           */
+        for (int i = 0; i < blocked.length; ++i) {
+            setBlocked(blocked[i][0], blocked[i][1]);
         }
 
-        this.aStar();
+        AStar();
+        List<DoubleVec> pathPositions = new ArrayList<>();
 
-        ArrayList<DoubleVec> positions = new ArrayList<>();
-        if(closed[endI][endJ]){
+        if (closed[endI][endJ]) {
+            //Trace back the path
+            System.out.println("Path: ");
             Cell current = grid[endI][endJ];
-            positions.add(new DoubleVec(current.i, current.j));
-            while(current.parent!=null){
-                positions.add(new DoubleVec(current.parent.i, current.parent.j));
+            System.out.print(current);
+            pathPositions.add(new DoubleVec(current.i, current.j));
+            while (current.parent != null) {
+                System.out.print(" -> " + current.parent);
+                pathPositions.add(new DoubleVec(current.parent.i, current.parent.j));
                 current = current.parent;
             }
-        } else {
-            System.out.println("No possible path");
+            System.out.println();
         }
-        return positions;
+        else System.out.println("No possible path");
+
+        return pathPositions;
     }
 
     /**
      * TODO: add JavaDoc
+     *
      * @param positions
      * @return
      */
-    public ArrayList<MoveDirection> getDirsList(ArrayList<DoubleVec> positions) {
-        ArrayList<MoveDirection> moveDirs = new ArrayList<>();
-        for(int i = 1; i < positions.size(); i++) {
-            DoubleVec diff = positions.get(i).sub(positions.get(i-1));
-            if(getMoveDirToVec(diff) != null) {
+    public static List<MoveDirection> getDirsList(List<DoubleVec> positions) {
+        List<MoveDirection> moveDirs = new ArrayList<>();
+        for (int i = 1; i < positions.size(); i++) {
+            DoubleVec diff = positions.get(i).sub(positions.get(i - 1));
+            if (getMoveDirToVec(diff) != null) {
                 moveDirs.add(getMoveDirToVec(diff));
-            } else {
+            }
+            else {
                 throw new IllegalArgumentException("Falscher Parameter bei getMoveDirToVec(diff) in AStar");
             }
         }
@@ -221,27 +206,36 @@ public class AStar {
 
     /**
      * TODO: add JavaDoc
+     *
      * @param vec
      * @return
      */
-    public MoveDirection getMoveDirToVec(DoubleVec vec) {
-        if(vec.equals(MoveDirection.UP.getVec())) {
+    public static MoveDirection getMoveDirToVec(DoubleVec vec) {
+        if (vec.equals(MoveDirection.UP.getVec())) {
             return MoveDirection.UP;
-        } else if(vec.equals(MoveDirection.DOWN.getVec())) {
+        }
+        else if (vec.equals(MoveDirection.DOWN.getVec())) {
             return MoveDirection.DOWN;
-        } else if(vec.equals(MoveDirection.LEFT.getVec())) {
+        }
+        else if (vec.equals(MoveDirection.LEFT.getVec())) {
             return MoveDirection.LEFT;
-        } else if(vec.equals(MoveDirection.RIGHT.getVec())) {
+        }
+        else if (vec.equals(MoveDirection.RIGHT.getVec())) {
             return MoveDirection.RIGHT;
-        } else if(vec.equals(MoveDirection.RIGHT_DOWN.getVec())) {
+        }
+        else if (vec.equals(MoveDirection.RIGHT_DOWN.getVec())) {
             return MoveDirection.RIGHT_DOWN;
-        } else if(vec.equals(MoveDirection.RIGHT_UP.getVec())) {
+        }
+        else if (vec.equals(MoveDirection.RIGHT_UP.getVec())) {
             return MoveDirection.RIGHT_UP;
-        } else if(vec.equals(MoveDirection.LEFT_DOWN.getVec())) {
+        }
+        else if (vec.equals(MoveDirection.LEFT_DOWN.getVec())) {
             return MoveDirection.LEFT_DOWN;
-        } else if(vec.equals(MoveDirection.LEFT_UP.getVec())) {
+        }
+        else if (vec.equals(MoveDirection.LEFT_UP.getVec())) {
             return MoveDirection.LEFT_UP;
-        } else {
+        }
+        else {
             return null;
         }
     }
--- Projekte/Tanks/src/main/java/pp/tanks/model/item/COMEnemy.java
+++ Projekte/Tanks/src/main/java/pp/tanks/model/item/COMEnemy.java
@@ -5,7 +5,10 @@ import pp.tanks.model.AStar;
 import pp.tanks.model.Model;
 import pp.util.DoubleVec;
 
+import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Represents a enemy played by the computer
@@ -14,10 +17,15 @@ public class COMEnemy extends Enemy {
     public final PlayerEnum player1 = PlayerEnum.PLAYER1;
     private long latestViewUpdate;
 
+    private List<DoubleVec> path;
+    private List<MoveDirection> dirs;
+
     protected COMEnemy(Model model, double effectiveRadius, Armor armor, Turret turret, TankData data) {
         super(model, effectiveRadius, armor, turret, data);
         if (model.getEngine() != null) latestViewUpdate = System.nanoTime() + model.getEngine().getOffset();
         else latestViewUpdate = System.nanoTime();
+        this.path = new ArrayList<>();
+        this.dirs = new ArrayList<>();
     }
 
     /**
@@ -65,13 +73,41 @@ public class COMEnemy extends Enemy {
             }
             else {*/
             if (!this.isDestroyed()) {
-                behaviour(delta);
+                // behaviour(delta);
             }
             //}
         }
-
         else interpolateTime(serverTime);
+
         latestViewUpdate = serverTime;
+
+        setMove(true);
+        if (!path.isEmpty() && !dirs.isEmpty()) {
+            updateMove(delta);
+            // setPos(getPos().add(getMoveDir().getVec().mult(delta * speed)));
+            if (getPos().distance(path.get(0)) < 0.5) {
+                setPos(path.get(0));
+                //setMoveDir()
+                setMoveDirection(dirs.get(0));
+                path.remove(0);
+                dirs.remove(0);
+            }
+        }
+        else if (path == null || path.isEmpty()) {
+            setMove(false);
+            if (!shootIsBlocked()) {
+                shoot(model.getTanksMap().getTank(PlayerEnum.PLAYER1).getPos());
+            }
+            double r = Math.random() * 4;
+            r -= 2;
+            r = Math.toIntExact(Math.round(r));
+            int x = Math.toIntExact(Math.round(model.getTanksMap().getTank(PlayerEnum.PLAYER1).getPos().x + r));
+            int y = Math.toIntExact(Math.round(model.getTanksMap().getTank(PlayerEnum.PLAYER1).getPos().y + r));
+            if (x != getPos().x || y != getPos().y) {
+                navigate(x, y);
+                setMoveDirection(dirs.get(0));
+            }
+        }
     }
 
     /**
@@ -104,33 +140,70 @@ public class COMEnemy extends Enemy {
         else return new Howitzer(model, data);
     }
 
-    /*
-    public void navigate(double x, double y) throws Exception {
+    public void navigate(int x, int y) {
         //TODO
-        double panzerX = getPos().x;
-        double panzerY = getPos().y;
-        double targetX = x;
-        double targetY = y;
-        int width = 5;
-        int height = 5;
-        //TODO: How to get Blocked list?? look in map
-        int[][] blocked = new int[][]{{0,4},{2,2},{3,1},{3,3}};
+        path.clear();
+        dirs.clear();
+        int panzerX = Math.toIntExact(Math.round(getPos().x));
+        int panzerY = Math.toIntExact(Math.round(getPos().y));
+        int width = model.getTanksMap().getWidth();
+        int height = model.getTanksMap().getHeight();
 
-        AStar aStar = new AStar();
-        ArrayList<DoubleVec> path =  aStar.getPath(width, height, panzerX, panzerY, targetX, targetY, blocked);
-        ArrayList<MoveDirection> dirs = aStar.getDirsList(path);
+        int n = model.getTanksMap().getBlocks().size() + model.getTanksMap().getAllTanks().size() - 1;
+        int[][] blocked = new int[n][2];
 
-        //TODO: Create Message to Server
-        setMove(true);
-        if(!path.isEmpty() && !dirs.isEmpty() &&getPos().distance(path.get(0)) < 0.1) {
-            setPos(path.get(0));
-            //setMoveDir()
-            setMoveDirection(dirs.get(0));
-            path.remove(0);
-            dirs.remove(0);
-        } else if(path.isEmpty()) {
-            setMove(false);
+        int i = 0;
+        while (i < model.getTanksMap().getBlocks().size()) {
+            blocked[i][1] = Math.toIntExact(Math.round(model.getTanksMap().getBlocks().get(i).getPos().x));
+            blocked[i][0] = Math.toIntExact(Math.round(model.getTanksMap().getBlocks().get(i).getPos().y));
+            i++;
         }
+
+        int j = 0;
+        while (i < n) {
+            if (model.getTanksMap().getAllTanks().get(j) != this) {
+                blocked[i][1] = Math.toIntExact(Math.round(model.getTanksMap().getAllTanks().get(j).getPos().x));
+                blocked[i][0] = Math.toIntExact(Math.round(model.getTanksMap().getAllTanks().get(j).getPos().y));
+                i++;
+            }
+            j++;
+        }
+
+
+        path = AStar.execute(height, width, panzerY, panzerX, y, x, blocked);
+        Collections.reverse(path);
+
+        List<DoubleVec> tmpPath = new ArrayList<>();
+        for (DoubleVec p : path) {
+            tmpPath.add(new DoubleVec(p.y, p.x));
+        }
+        path = tmpPath;
+
+        dirs = AStar.getDirsList(path);
+        Collections.reverse(dirs);
+
+        // path.remove(0);
     }
+
+    /**
+     * test if a ComEnemy can hit the players Tank if he would shoot
+     *
+     * @return if the way is blocked
      */
+    public boolean shootIsBlocked() {
+        DoubleVec pos = model.getTanksMap().getTank(PlayerEnum.PLAYER1).getPos();
+        DoubleVec dir = getPos().sub(pos).normalize().mult(0.5);
+        while (pos.distance(getPos()) < 0.1) {
+            for (Block other : model.getTanksMap().getBlocks()) {
+                Block block = (Block) other;
+                Ellipse2D item1 = new Ellipse2D.Double(pos.x - (effectiveRadius / 2), pos.y - (effectiveRadius / 2), effectiveRadius, effectiveRadius);
+                if (item1.intersects(other.getPos().x - (block.getWidth() / 2.0),
+                                     other.getPos().y - (block.getHeight() / 2.0), block.getWidth(), block.getHeight())) {
+                    return true;
+                }
+                pos = pos.add(dir);
+            }
+        }
+        return false;
+    }
 }
