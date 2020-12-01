7294ea90e9b1d6904cae3468ca7984f5cdce42b2
Georg Haindl <georg.haindl@unibw.de>
2020-12-01 18:04:55 +0100

    added new tests modified some methods for tests

--- Projekte/Tanks/src/main/java/pp/tanks/model/item/Tank.java
+++ Projekte/Tanks/src/main/java/pp/tanks/model/item/Tank.java
@@ -276,18 +276,18 @@ public abstract class Tank extends Item<TankData> {
     /**
      * handles the movement of the tank if it collides with other tanks or blocks in the map
      */
-    protected boolean collide(DoubleVec pos) {
+    public boolean collide(DoubleVec pos) {
         for (Tank tank : model.getTanksMap().getAllTanks()) {
             if (this != tank && collisionWith(tank, pos)) {
                 //setPos(getPos().sub(getMoveDir().getVec().mult(0.01)));
-                setMoveDirection(STAY);
+                stopMovement();
                 setMove(false);
                 return true;
             }
         }
         for (Block block : model.getTanksMap().getBlocks()) {
             if (collisionWith(block, pos)) {
-                setMoveDirection(STAY);
+                stopMovement();
                 setMove(false);
                 return true;
             }
--- Projekte/Tanks/src/test/java/pp/tanks/SingleplayerTest.java
+++ Projekte/Tanks/src/test/java/pp/tanks/SingleplayerTest.java
@@ -1,11 +1,8 @@
 package pp.tanks;
 
-import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import pp.tanks.client.TanksApp;
-import pp.tanks.controller.Engine;
 import pp.tanks.message.data.Data;
 import pp.tanks.message.data.DataTimeItem;
 import pp.tanks.message.data.ProjectileData;
@@ -38,7 +35,7 @@ public class SingleplayerTest {
 
     @Disabled
     public void movementTest() {
-        player = new PlayersTank(model, 0.3, new HeavyArmor(), new LightTurret(),
+        player = new PlayersTank(model, 0.3, new LightArmor(), new LightTurret(),
                 new TankData(new DoubleVec(3, 6), 0, 100, MoveDirection.STAY, 0.0, new DoubleVec(0, 1), false)); //TODO: multiply
         model.getTanksMap().addPlayerTank(player);
         assertEquals(new DoubleVec(3, 6), player.getPos());
@@ -50,61 +47,127 @@ public class SingleplayerTest {
     }
 
     @Test
-    public void lightTurretTest() {  //TODO: unvollständig
+    public void lightTurretTest() {
         long time;
         player = new PlayersTank(model, 0.3, new HeavyArmor(), new LightTurret(),
                 new TankData(new DoubleVec(3, 6), 0, 100, MoveDirection.STAY, 0.0, new DoubleVec(0, 1), false)); //TODO: multiply
         model.getTanksMap().addPlayerTank(player);
+        assertEquals(5, model.getTanksMap().getAllTanks().get(0).getTurret().getMagSize());
+        assertTrue(model.getTanksMap().getAllTanks().get(0).getTurret().canShoot());
         assertEquals(0, model.getTanksMap().getProjectiles().size());
+
         player.shoot(new DoubleVec(10, 6));
         assertFalse(model.getTanksMap().getAddedProjectiles().isEmpty());
+
         time = System.nanoTime();
         Projectile firstShot = model.getTanksMap().getAddedProjectiles().get(1001);
         model.getTanksMap().getHashProjectiles().put(firstShot.getData().id, firstShot);
         DataTimeItem<ProjectileData> tmp = new DataTimeItem<>(firstShot.getData().mkCopy(), time);
         assertEquals(1, model.getTanksMap().getProjectiles().size());
+
         firstShot.interpolateData(tmp);
         firstShot.interpolateTime(time + 1_100_000_000);
         assertEquals(new DoubleVec(9.51, 6), model.getTanksMap().getProjectiles().get(0).getPos());
+
+        assertEquals(ItemEnum.LIGHT_PROJECTILE, model.getTanksMap().getProjectiles().get(0).getProjectileData().type);
     }
 
     @Test
-    public void normalTurretTest() { //TODO: unvollständig
+    public void normalTurretTest() {
         long time;
         assertEquals(0, model.getTanksMap().getProjectiles().size());
         player = new PlayersTank(model, 0.3, new HeavyArmor(), new NormalTurret(),
                 new TankData(new DoubleVec(3, 6), 0, 100, MoveDirection.STAY, 0.0, new DoubleVec(0, 1), false));
         model.getTanksMap().addPlayerTank(player);
+        assertEquals(3, model.getTanksMap().getAllTanks().get(0).getTurret().getMagSize());
+        assertTrue(model.getTanksMap().getAllTanks().get(0).getTurret().canShoot());
         assertEquals(0, model.getTanksMap().getProjectiles().size());
+
         player.shoot(new DoubleVec(10, 6));
         assertFalse(model.getTanksMap().getAddedProjectiles().isEmpty());
+
         time = System.nanoTime();
         Projectile firstShot = model.getTanksMap().getAddedProjectiles().get(1001);
         model.getTanksMap().getHashProjectiles().put(firstShot.getData().id, firstShot);
         DataTimeItem<ProjectileData> tmp = new DataTimeItem<>(firstShot.getData().mkCopy(), time);
         assertEquals(1, model.getTanksMap().getProjectiles().size());
+
         firstShot.interpolateData(tmp);
         firstShot.interpolateTime(time + 1_100_000_000);
         assertEquals(new DoubleVec(9.51, 6), model.getTanksMap().getProjectiles().get(0).getPos());
+
+        assertEquals(ItemEnum.NORMAL_PROJECTILE, model.getTanksMap().getProjectiles().get(0).getProjectileData().type);
+
     }
 
     @Test
-    public void HeavyTurretTest() {  //TODO: unvollständig
+    public void heavyTurretTest() {
         long time;
         assertEquals(0, model.getTanksMap().getProjectiles().size());
         player = new PlayersTank(model, 0.3, new HeavyArmor(), new HeavyTurret(),
                 new TankData(new DoubleVec(3, 6), 0, 100, MoveDirection.STAY, 0.0, new DoubleVec(0, 1), false));
         model.getTanksMap().addPlayerTank(player);
+        assertEquals(1, model.getTanksMap().getAllTanks().get(0).getTurret().getMagSize());
+        assertTrue(model.getTanksMap().getAllTanks().get(0).getTurret().canShoot());
         assertEquals(0, model.getTanksMap().getProjectiles().size());
+
         player.shoot(new DoubleVec(10, 6));
         assertFalse(model.getTanksMap().getAddedProjectiles().isEmpty());
+
         time = System.nanoTime();
         Projectile firstShot = model.getTanksMap().getAddedProjectiles().get(1001);
         model.getTanksMap().getHashProjectiles().put(firstShot.getData().id, firstShot);
         DataTimeItem<ProjectileData> tmp = new DataTimeItem<>(firstShot.getData().mkCopy(), time);
         assertEquals(1, model.getTanksMap().getProjectiles().size());
+
         firstShot.interpolateData(tmp);
         firstShot.interpolateTime(time + 1_100_000_000);
         assertEquals(new DoubleVec(9.51, 6), model.getTanksMap().getProjectiles().get(0).getPos());
+
+        assertEquals(ItemEnum.HEAVY_PROJECTILE, model.getTanksMap().getProjectiles().get(0).getProjectileData().type);
+
+    }
+
+    @Test
+    public void collisionBlockTest() {
+        player = new PlayersTank(model, 0.3, new HeavyArmor(), new HeavyTurret(),
+                new TankData(new DoubleVec(4.5, 5), 0, 100, MoveDirection.STAY, 0.0, new DoubleVec(0, 1), false));
+        model.getTanksMap().addPlayerTank(player);
+        assertFalse(player.collide(player.getPos()));
+        UnbreakableBlock uBlock = new UnbreakableBlock(model, new Data(new DoubleVec(5, 5), 10, false));
+        model.getTanksMap().addUnbreakableBlock(uBlock);
+        assertTrue(player.collide(player.getPos()));
+    }
+
+    @Test
+    public void collisionEnemyTest(){
+        player = new PlayersTank(model, 0.3, new HeavyArmor(), new HeavyTurret(),
+                new TankData(new DoubleVec(4.5, 5), 0, 100, MoveDirection.STAY, 0.0, new DoubleVec(0, 1), false));
+        model.getTanksMap().addPlayerTank(player);
+        assertFalse(player.collide(player.getPos()));
+        COMEnemy enemy = new ArmoredPersonnelCarrier(model,
+                new TankData(new DoubleVec(5,5), 1, 100, MoveDirection.STAY, 0.0, new DoubleVec(0,1), false));
+        model.getTanksMap().addCOMTank(enemy);
+        assertTrue(player.collide(player.getPos()));
+    }
+
+    @Test
+    public void tankDestroyed(){
+        player = new PlayersTank(model, 0.3, new HeavyArmor(), new HeavyTurret(),
+                new TankData(new DoubleVec(4.5, 5), 0, 100, MoveDirection.STAY, 0.0, new DoubleVec(0, 1), false));
+        model.getTanksMap().addPlayerTank(player);
+        assertEquals(player, model.getTanksMap().getAllTanks().get(0));
+        player.processDamage(100);
+        assertTrue(player.getData().isDestroyed());
+    }
+
+    @Test
+    public void enemyDestroyed(){
+        COMEnemy enemy = new ArmoredPersonnelCarrier(model,
+                new TankData(new DoubleVec(5,5), 1, 100, MoveDirection.STAY, 0.0, new DoubleVec(0,1), false));
+        model.getTanksMap().addCOMTank(enemy);
+        assertEquals(enemy, model.getTanksMap().getAllTanks().get(0));
+        enemy.processDamage(100);
+        assertTrue(enemy.getData().isDestroyed());
     }
 }
